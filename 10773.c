fd830d114b1df525177b7602746068b8b79948d1
doja3045 <108964346+doja3045@users.noreply.github.com>
2022-07-09 12:07:51 +0900

    10773
    최대한 열심히 했는ㄴ데 틀렸어 ㅜㅜ

---
+++ 10773.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int a,b;
+    scanf("%d",&a);
+    int *p = (int*)malloc(sizeof(int) * a);
+    for(int i = 0; i < a; i++){
+        scanf("%d",&p[i]);
+    }
+    for(int i = 0; i < a; i++){
+        if(p[i] == 0){
+            continue;
+        }
+        else if(p[i] + p[i+1] == p[i]){
+            p[i] = 0;
+        }
+    }
+    for(int i = 0; i < a; i++){
+        b = b + p[i];
+    }
+    printf("%d",b);
+}
